72bcf62b65b9fec3dee6843add07bee8a6607d4f
Daniel Wanner <daniel.wanner@pm.me>
2021-06-23 11:20:19 +0200

    Added env file

---
+++ .env.zsh
@@ -0,0 +1,10 @@
+# PATH extensions
+export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME/.local/share/gem/ruby/3.0.0/bin:$PATH
+export PATH=$HOME/dockergento/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.emacs.d/bin:$PATH
+
+# SSH-AGENT FIX
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
